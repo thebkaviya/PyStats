87d21615e8e53c085ae85ef3a3e5e3441ab4b95e
Binula Kavisinghe <bkavisinghe@gmail.com>
2022-10-10 21:03:47 +0530

    Create system.py

---
+++ system.py
@@ -0,0 +1,18 @@
+import psutil
+import platform
+from datetime import datetime
+
+print("="*40, "System Information", "="*40)
+uname = platform.uname()
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print(f"Processor: {uname.processor}")
+
+# Boot Time
+print("="*40, "Boot Time", "="*40)
+boot_time_timestamp = psutil.boot_time()
+bt = datetime.fromtimestamp(boot_time_timestamp)
+print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
