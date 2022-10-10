79c7b8449d6060eda3acc4b61d0094ac5014e871
Binula Kavisinghe <bkavisinghe@gmail.com>
2022-10-10 21:06:55 +0530

    Create cpu.py

---
+++ cpu.py
@@ -0,0 +1,16 @@
+import psutil
+
+print("="*40, "CPU Info", "="*40)
+# number of cores
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+# CPU frequencies
+cpufreq = psutil.cpu_freq()
+print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+# CPU usage
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
