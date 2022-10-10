e0e3a6325e115b042f65e179c16a6e6f166ac2c7
Binula Kavisinghe <bkavisinghe@gmail.com>
2022-10-10 21:31:32 +0530

    Update memory.py

---
+++ memory.py
@@ -0,0 +1,15 @@
+# Memory Information
+print("="*40, "Memory Information", "="*40)
+# get the memory details
+svmem = psutil.virtual_memory()
+print(f"Total: {get_size(svmem.total)}")
+print(f"Available: {get_size(svmem.available)}")
+print(f"Used: {get_size(svmem.used)}")
+print(f"Percentage: {svmem.percent}%")
+print("="*20, "SWAP", "="*20)
+# get the swap memory details (if exists)
+swap = psutil.swap_memory()
+print(f"Total: {get_size(swap.total)}")
+print(f"Free: {get_size(swap.free)}")
+print(f"Used: {get_size(swap.used)}")
+print(f"Percentage: {swap.percent}%")
